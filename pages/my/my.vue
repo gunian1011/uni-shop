66c4c3bf03eec37f4fb50f5263d86c1283c4808e
顾念\51786 <gunian1011@163.com>
2022-11-19 20:31:19 +0800

    完成登录和支付的开发，不能正常使用

--- pages/my/my.vue
+++ pages/my/my.vue
@@ -1,22 +1,35 @@
 <template>
-	<view>
-		My
+	<view class="my-container">
+		<!-- <my-login v-if="!token"></my-login>
+    <my-userinfo v-else></my-userinfo> -->
+    
+    <my-login v-if="!token"></my-login>
+    <my-userinfo v-else></my-userinfo>
+
 	</view>
 </template>
 
 <script>
 // 导入自己封装的 mixin 模块
 import badgeMix from '@/mixins/tabbar-badge.js'
+import { mapState } from 'vuex'
 export default {
     mixins: [badgeMix],
 		data() {
 			return {
 				
 			};
-		}
+		},
+    computed: {
+      ...mapState('m_user',['token'])
+    }
+    
 	}
 </script>
 
 <style lang="scss">
-
+page,
+.my-container {
+  height: 100%;
+}
 </style>
